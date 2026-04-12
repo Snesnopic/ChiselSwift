d3aa19b4d84462c73ddefdab8f03db1eba56df26
Snesnopic <snesnopic@gmail.com>
2026-04-12 21:40:41 +0200

    Initial Commit

---
+++ testchisel/testchiselApp.swift
@@ -0,0 +1,17 @@
+//
+//  testchiselApp.swift
+//  testchisel
+//
+//  Created by Giuseppe Francione on 12/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct testchiselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
